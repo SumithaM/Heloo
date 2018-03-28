bd97a8ad87d56be7837e54a62101599a384d6ce3
SumithaM <sumithaamoorthi@gmail.com>
2018-03-28 11:55:52 +0530

    Added sysout

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,7 +3,7 @@ package com.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Sample code next");
+		System.out.println("Sample code next in ");
 
 	}
 
